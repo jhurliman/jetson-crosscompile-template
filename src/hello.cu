5fd0a68b22333a319474e9c674e918cbb75e6cf4
John Hurliman <jhurliman@jhurliman.org>
2023-12-30 21:00:01 -0800

    Add .clang-format

--- src/hello.cu
+++ src/hello.cu
@@ -1,14 +1,15 @@
-#include <cstdio>
 #include <cuda_runtime.h>
 
+#include <cstdio>
+
 __global__ void helloKernel() {
-    printf("Hello from GPU!\n");
+  printf("Hello from GPU!\n");
 }
 
 void helloFromGPU(cudaStream_t stream) {
-    // Launch a kernel with a single thread to print from the GPU
-    helloKernel<<<1, 1, 0, stream>>>();
+  // Launch a kernel with a single thread to print from the GPU
+  helloKernel<<<1, 1, 0, stream>>>();
 
-    // Wait for the GPU to finish before returning to the host
-    cudaStreamSynchronize(stream);
+  // Wait for the GPU to finish before returning to the host
+  cudaStreamSynchronize(stream);
 }
--- src/test.cpp
+++ src/test.cpp
@@ -1,12 +1,12 @@
-#include <iostream>
-
 #include <cuda_runtime.h>
+
+#include <iostream>
 
 // CUDA forward declaration
 void helloFromGPU(cudaStream_t stream);
 
 int main() {
-  std::cout << "Hello from CPU!" << std::endl;
+  std::cout << "Hello from CPU!\n";
 
   // Call a function that runs on the GPU
   helloFromGPU(nullptr);
