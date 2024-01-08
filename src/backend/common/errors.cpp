ec694aef5cf2b3a4082cd0a5115ce87f1ea6aecf
John Hurliman <jhurliman@jhurliman.org>
2024-01-08 13:27:46 -0800

    Fix clang-tidy warnings in the signal handler

--- src/backend/common/errors.cpp
+++ src/backend/common/errors.cpp
@@ -5,12 +5,18 @@
 #include <string.h> // for strsignal
 #include <unistd.h> // for STDERR_FILENO, write()
 
+#include <array>
+
 static void SignalHandler(int signalNum) {
   const char* signalName = ::strsignal(signalNum);
 
-  char buffer[512];
-  int len = ::snprintf(buffer, sizeof(buffer), "Caught signal: %d (%s)\n", signalNum, signalName);
-  if (len > 0) { ::write(STDERR_FILENO, buffer, size_t(len)); }
+  constexpr size_t BUFFER_SIZE = 512;
+  std::array<char, BUFFER_SIZE> buffer{};
+  // NOLINTBEGIN(cppcoreguidelines-pro-type-vararg)
+  int len =
+    ::snprintf(buffer.data(), buffer.size(), "Caught signal: %d (%s)\n", signalNum, signalName);
+  // NOLINTEND(cppcoreguidelines-pro-type-vararg)
+  if (len > 0) { ::write(STDERR_FILENO, buffer.data(), size_t(len)); }
 
   cpptrace::generate_trace(2).print(); // Skip this function and the signal handler
   exit(signalNum);
