4b1d270858cbfd6c88f6a47e08bb0a3046ce3ef8
John Hurliman <jhurliman@jhurliman.org>
2024-01-08 12:45:39 -0800

    Fix jetson-nano build by including <unistd.h>

--- src/backend/common/errors.cpp
+++ src/backend/common/errors.cpp
@@ -3,13 +3,14 @@
 #include <cpptrace/cpptrace.hpp>
 #include <signal.h>
 #include <string.h> // for strsignal
+#include <unistd.h> // for STDERR_FILENO, write()
 
 static void SignalHandler(int signalNum) {
-  const char* signalName = strsignal(signalNum);
+  const char* signalName = ::strsignal(signalNum);
 
   char buffer[512];
-  int len = snprintf(buffer, sizeof(buffer), "Caught signal: %d (%s)\n", signalNum, signalName);
-  if (len > 0) { write(STDERR_FILENO, buffer, size_t(len)); }
+  int len = ::snprintf(buffer, sizeof(buffer), "Caught signal: %d (%s)\n", signalNum, signalName);
+  if (len > 0) { ::write(STDERR_FILENO, buffer, size_t(len)); }
 
   cpptrace::generate_trace(2).print(); // Skip this function and the signal handler
   exit(signalNum);
