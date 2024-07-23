99fb0aeaa786aac68b472ce8fa5d414ae25ea902
John Hurliman <jhurliman@jhurliman.org>
2024-07-23 15:12:34 -0700

    Add partial fix for building CUDA host target on macOS

--- host.toolchain.cmake
+++ host.toolchain.cmake
@@ -3,13 +3,26 @@ set(CMAKE_C_COMPILER "clang")
 set(CMAKE_CUDA_COMPILER "clang++")
 set(CMAKE_CXX_COMPILER "clang++")
 
-# Find the latest host (x86_64) vendored CUDA toolkit
-if(EXISTS "${CMAKE_CURRENT_LIST_DIR}/nvidia/cuda-11.4_amd64")
-  set(CUDAToolkit_ROOT "${CMAKE_CURRENT_LIST_DIR}/nvidia/cuda-11.4_amd64")
-elseif(EXISTS "${CMAKE_CURRENT_LIST_DIR}/nvidia/cuda-10.2_amd64")
-  set(CUDAToolkit_ROOT "${CMAKE_CURRENT_LIST_DIR}/nvidia/cuda-10.2_amd64")
+if(APPLE)
+  # Use the only CUDA toolkit available for macOS (10.2)
+  set(CUDAToolkit_ROOT "${CMAKE_CURRENT_LIST_DIR}/nvidia/cuda-10.2_macos")
+  if(NOT EXISTS ${CUDAToolkit_ROOT})
+    message(
+      FATAL_ERROR
+        "CUDAToolkit_ROOT does not exist: ${CUDAToolkit_ROOT}\nPlease run ./scripts/extract-cuda.sh")
+  endif()
+
+  # FIXME: This doesn't fully work because the CUDA test program is compiled with `-lrt` which is
+  # not available on macOS
 else()
-  message(FATAL_ERROR "CUDAToolkit_ROOT does not exist. Please run ./scripts/extract-cuda.sh")
+  # Find the latest host (x86_64) vendored CUDA toolkit
+  if(EXISTS "${CMAKE_CURRENT_LIST_DIR}/nvidia/cuda-11.4_amd64")
+    set(CUDAToolkit_ROOT "${CMAKE_CURRENT_LIST_DIR}/nvidia/cuda-11.4_amd64")
+  elseif(EXISTS "${CMAKE_CURRENT_LIST_DIR}/nvidia/cuda-10.2_amd64")
+    set(CUDAToolkit_ROOT "${CMAKE_CURRENT_LIST_DIR}/nvidia/cuda-10.2_amd64")
+  else()
+    message(FATAL_ERROR "CUDAToolkit_ROOT does not exist. Please run ./scripts/extract-cuda.sh")
+  endif()
 endif()
 
 set(CUDA_SEPARABLE_COMPILATION ON CACHE BOOL "Enable separable compilation for CUDA")
